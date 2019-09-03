d42d34b15d18afd4113e0bc9a01c8c12b6cb6dff
范文华 <531362022@qq.com>
2019-09-03 09:39:26 +0800

    feat: Generate signature file

---
+++ README.md
@@ -0,0 +1,19 @@
+# react-native-android-sign
+对android一键签名文件，拒绝繁琐
+
+# 安装
+```bash
+# Npm
+npm install react-native-android-sign --save-dev
+
+# Yarn
+yarn add react-native-android-sign --dev
+```
+
+# 使用
+```bash
+npx android-sign
+```
+
+# 注意事项
+生成后签名后，会打印出一串配置，您需要将这段配置复制到文件`.android/app/build.gradle`中
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import path from 'path';
+import fs from 'fs';
+import { execSync } from 'child_process';
+import uniqueString from 'unique-string';
+import inquirer from 'inquirer';
+import colors from 'colors';
+
+// CN 您的名字与姓氏是什么?
+// OU 您的组织单位名称是什么?
+// O  您的组织名称是什么?
+// L  您所在的城市或区域名称是什么?
+// ST 您所在的省/市/自治区名称是什么?
+// C  该单位的双字母国家/地区代码是什么?
+
+const signPath = path.resolve('android/release.keystore');
+
+const createSignature = () => {
+    const storePassword = uniqueString();
+    const keyPassword = uniqueString();
+    const alias = path.basename(process.cwd());
+
+    execSync(`keytool -genkey -v -storetype PKCS12 -keyalg RSA -keysize 2048 -validity 10000 -keystore "${signPath}" -storepass "${storePassword}" -alias "${alias}" -keypass "${keyPassword}" -dname "CN=NoName, O=NoOrg, C=NoCountry"`);
+
+    console.log('');
+    console.log(colors.bgBlack(colors.white('Copy green color code from below to file: ./android/app/build.gradle')));
+    console.log('android {\n    ...');
+    console.log(colors.green(`
+    signingConfigs {
+        release {
+            storeFile file(../release.keystore)
+            storePassword "${storePassword}"
+            keyAlias "${alias}"
+            keyPassword "${keyPassword}"
+        }
+    }
+    `));
+    console.log('    ...\n}');
+};
+
+if (fs.existsSync(signPath)) {
+    inquirer.createPromptModule()<{ override: boolean }>({
+        type: 'list',
+        name: 'override',
+        message: '\nHere exists a signature file, do you want to override it?\n',
+        prefix: '',
+        suffix: '',
+        choices: [
+            {
+                name: '1. No. Stop doing that',
+                value: false,
+            },
+            {
+                name: '2. Yes, I want to override\n',
+                value: true,
+            },
+        ],
+    }).then(({ override }) => {
+        if (override) {
+            fs.unlinkSync(signPath);
+            createSignature();
+        }
+    });
+} else {
+    createSignature();
+}
